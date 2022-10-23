af99309402f1d677efb6a4ad31a54289a8df89b1
Bayan Khalighi <30418939+bayank@users.noreply.github.com>
2022-10-23 16:04:50 -0400

    Initial

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,38 @@
+
+const val HERO_NAME = "Madrigal"
+fun main() {
+    println("The hero announces her presence to the world.")
+
+    println(HERO_NAME)
+    var playerLevel = 10
+    println(playerLevel)
+    val hasBefriendedBarbarians = true
+    val hasAngeredBarbarians = false
+    val playerClass = "paladin"
+    val quest: String = when (playerLevel) {
+        1 -> "Meet Mr. Bubbles in the land of soft things."
+        in 2..5 -> {
+            // Check if diplomacy is an option
+            val canTalkToBarbarians = !hasAngeredBarbarians &&
+                    (hasBefriendedBarbarians || playerClass == "barbarian")
+            if (canTalkToBarbarians) {
+                "Convince the barbarians to call of their invasion."
+            } else {
+                "Save the town from the barbarian invasions."
+            }
+        }
+        6 -> "Locate the enchanted sword."
+        7 -> "Recover the long-lost artifact of creation."
+        8 -> "Defeat Nogartse, bringer of death and eater of worlds."
+        else -> "There are no quests right now."
+        }
+
+    println("The hero approaches the bounty board. It reads:")
+    println(quest)
+
+    println("Time passes...")
+    println("The hero returns from her quest")
+    playerLevel += 1
+    println(playerLevel)
+
+}
